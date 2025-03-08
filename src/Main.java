1b4b1ed28d50d5216c8903e74c0efdbcf57ad798
Mehmet Ali Tezcan <mehmetalitez33@gmail.com>
2025-03-08 16:18:05 +0300

    Güncelleme

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,26 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Armut Kaç Kilo ? :");
+        double armut = scanner.nextDouble();
+        System.out.println("Elma Kaç Kilo ? :");
+        double elma = scanner.nextDouble();
+        System.out.println("Domates Kaç Kilo ? :");
+        double domates = scanner.nextDouble();
+        System.out.println("Muz Kaç Kilo ? :");
+        double muz = scanner.nextDouble();
+        System.out.println("Patlıcan Kaç Kilo ? :");
+        double patlican = scanner.nextDouble();
+
+        double tutar=armut * 2.14 + elma*3.67+ domates*1.11 + muz*0.95 + patlican *5.00;
+        System.out.println("Toplam Tutar: " + tutar +"TL");
+
+
+
     }
 }
